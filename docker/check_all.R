e861509a379a023ed0fee290145c1f82b7e4c401
hansvancalster <hans.vancalster@inbo.be>
2022-11-25 16:00:15 +0100

    update protocolhelper and check all

--- docker/check_all.R
+++ docker/check_all.R
@@ -1,28 +1,23 @@
 # bundled checks for protocols
 library(protocolhelper)
 check_all <- function(protocol_code) {
-  test1 <-
+  check_fm <-
     tryCatch(
       protocolhelper::check_frontmatter(protocol_code),
       error = function(e) e
     )
-  test2 <-
+  check_str <-
     tryCatch(
       protocolhelper::check_structure(protocol_code),
       error = function(e) e
     )
-  fail <-
-    inherits(test1, "error") | inherits(test2, "error")
-  if (fail) {
-    stop(
-      sprintf("\nThe source code failed some checks.
-              Please check the error messages:
-              error in check_frontmatter: %1$s
-              error in check_structure: %2$s",
-              test1$message,
-              test2$message
-              )
-      )
+  if (inherits(check_fm, "error") ) {
+    stop(sprintf("One or more errors occurred in check_frontmatter: %s \n",
+                    check_fm$message))
+  }
+  if (inherits(check_str, "error")) {
+    stop(sprintf("One or more errors occurred in check_frontmatter: %s \n",
+            check_str$message))
   }
 }
 check_all(Sys.getenv("GITHUB_HEAD_REF"))  
