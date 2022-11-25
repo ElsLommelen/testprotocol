f91c43c049b28693497f87b9b093e5ca4565abb7
hansvancalster <hans.vancalster@inbo.be>
2022-11-25 16:01:12 +0100

    forgot to save

--- docker/check_all.R
+++ docker/check_all.R
@@ -11,13 +11,14 @@ check_all <- function(protocol_code) {
       protocolhelper::check_structure(protocol_code),
       error = function(e) e
     )
-  if (inherits(check_fm, "error") ) {
-    stop(sprintf("One or more errors occurred in check_frontmatter: %s \n",
-                    check_fm$message))
-  }
-  if (inherits(check_str, "error")) {
-    stop(sprintf("One or more errors occurred in check_frontmatter: %s \n",
-            check_str$message))
+  if (inherits(check_fm, "error") | inherits(check_str, "error")) {
+    stop(
+      sprintf(
+        "One or more errors occurred in either check_frontmatter or
+        check_structure: %s \n",
+        c(check_fm$message, check_str$message)
+      )
+    )
   }
 }
 check_all(Sys.getenv("GITHUB_HEAD_REF"))  
